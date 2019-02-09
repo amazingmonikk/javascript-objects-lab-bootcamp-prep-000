c920465e9131bb837127df75b003ee5a7528b4a6
Monika Tadross <monikk.tadross@gmail.com>
2019-02-09 03:58:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var recipe = {key: 'value'};
-  var newObject = Object.assign({}, object, {})
+  var newObject = Object.assign({}, object, {[key]})
  
  
    //deletes `key` from a clone of object and returns the new object (it is non-destructive):
